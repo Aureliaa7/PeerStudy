13365b169c097d6c23cec335abf71cecc9e2eae7
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2023-02-11 22:57:23 +0200

    Handle unhandled exceptions that occur within a component

--- PeerStudy/PeerStudy/Shared/MainLayout.razor.cs
+++ PeerStudy/PeerStudy/Shared/MainLayout.razor.cs
@@ -1,5 +1,6 @@
 ﻿using MatBlazor;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using PeerStudy.Services.Interfaces;
 using System;
 
@@ -10,12 +11,20 @@ namespace PeerStudy.Shared
         [Inject]
         INavigationMenuService NavigationMenuService { get; set; }
 
+        private ErrorBoundary errorBoundary;
+        private bool showErrorAlert = true;
 
         MatTheme matBlazorTheme = new MatTheme()
         {
             Primary = (new MatThemeColorShadow("blue_primary", "blue1", "#3B7197")).Value,
             Secondary = (new MatThemeColorShadow("blue_secondary", "blue2", "#74BDE0")).Value
         };
+
+        // Note: We need to call the 'Recover' method so that the error message gets vanished when navigating to other pages
+        protected override void OnParametersSet()
+        {
+            errorBoundary?.Recover();
+        }
 
         protected override void OnInitialized()
         {
